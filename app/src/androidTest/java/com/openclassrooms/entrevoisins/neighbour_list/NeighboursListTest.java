a95aed564150e3886bcc59a30cef9053026e3d8f
Florent Barbaud <barbaudflorent@gmail.com>
2020-04-11 16:55:22 +0200

    Ajout du test affichage info user

--- app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
+++ app/src/androidTest/java/com/openclassrooms/entrevoisins/neighbour_list/NeighboursListTest.java
@@ -5,6 +5,7 @@ import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.ui.neighbour_list.ListNeighbourActivity;
@@ -16,9 +17,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.openclassrooms.entrevoisins.utils.RecyclerViewItemCountAssertion.withItemCount;
 import static org.hamcrest.core.IsNull.notNullValue;
 
@@ -67,5 +71,15 @@ public class NeighboursListTest {
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, new DeleteViewAction()));
         // Then : the number of element is 11
         onView(ViewMatchers.withId(R.id.list_neighbours)).check(withItemCount(ITEMS_COUNT-1));
+    }
+
+    @Test
+    public void myNeighboursListGoToInfoNeighbours() {
+        // Click sur Neighbours Jack
+        onView(ViewMatchers.withId(R.id.list_neighbours))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(1,click()));
+        // affichage de Jack sur info Neighbour
+        onView(ViewMatchers.withId(R.id.activity_info_neighbour_name2_tkt))
+                .check(matches(withText("Jack")));
     }
 }
--- app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourApiService.java
@@ -58,11 +58,9 @@ public class DummyNeighbourApiService implements  NeighbourApiService {
         if (!neighbour.getFavorites()){
                 neighbour.setFavorites(true);
                 favorites.add(neighbour);
-                //Log.i("DATA", "favoritesNeighbour: modification favoris true "+neighbour.getName());
         }
         else {
             neighbour.setFavorites(false);
-            //Log.i("DATA", "favoritesNeighbour: modification favoris false " +neighbour.getName());
             favorites.remove(neighbour);
         }
 
